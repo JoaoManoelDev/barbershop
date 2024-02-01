b44b6c36a30ddec5203ff9f5faadafa085a44cd6
João Manoel <joaomanoeldev@gmail.com>
2024-02-01 17:53:55 -0300

    feat: add header to home

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import { Header } from "@/components/header"
+
+const Home = () => {
+  return (
+    <div className="max-w-6xl mx-auto">
+      <Header />
+    </div>
+  )
+}
+
+export default Home
