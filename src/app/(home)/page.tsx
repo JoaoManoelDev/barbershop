6b1afe513dbd836dcacbecf4fddf3aeaf68b5404
João Manoel <joaomanoeldev@gmail.com>
2024-02-02 01:42:40 -0300

    feat: add welcome and search barber component to home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,16 @@
+import { SearchBarber } from "./components/search-barber"
+import { Welcome } from "./components/welcome"
 import { Header } from "@/components/header"
 
 const Home = () => {
   return (
-    <div className="max-w-6xl mx-auto">
+    <div className="max-w-6xl mx-auto container">
       <Header />
+
+      <div className="space-y-6 py-5 ">
+        <Welcome />
+        <SearchBarber />
+      </div>
     </div>
   )
 }
