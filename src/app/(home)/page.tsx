175a468677e53092266b1fa276f80a0c6696f86f
João Manoel <joaomanoeldev@gmail.com>
2024-02-02 03:11:34 -0300

    feat: add booking item component to home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import { SearchBarber } from "./components/search-barber"
 import { Welcome } from "./components/welcome"
 import { Header } from "@/components/header"
+import { BookingItem } from "@/components/booking-item"
 
 const Home = () => {
   return (
@@ -10,6 +11,10 @@ const Home = () => {
       <div className="space-y-6 py-5 ">
         <Welcome />
         <SearchBarber />
+        <div className="gap-2">
+          <p className="text-xs uppercase font-bold text-muted-foreground mb-3">Agendamentos</p>
+          <BookingItem />
+        </div>
       </div>
     </div>
   )
