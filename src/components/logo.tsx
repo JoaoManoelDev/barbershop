c26ec59b4ee3117907f9a9be6086697845e47b8b
João Manoel <joaomanoeldev@gmail.com>
2024-02-03 19:15:29 -0300

    feat: add link to logo component

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,10 +1,14 @@
 import { Icons } from "@/components/icons"
+import Link from "next/link"
 
 export const Logo = () => {
   return (
-    <div className="text-2xl font-bold font-kalam tracking-tighter flex  gap-1">
+    <Link
+      href="/"
+      className="text-2xl font-bold font-kalam tracking-tighter flex gap-1"
+    >
       <Icons.scissors className="w-7 h-7" />
       <span>Barbershop</span>
-    </div>
+    </Link>
   )
 }
