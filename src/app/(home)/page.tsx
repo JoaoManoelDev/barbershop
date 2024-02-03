2ff5e55825f3da377a8e5af400e8cb4fe8765eef
João Manoel <joaomanoeldev@gmail.com>
2024-02-02 22:31:21 -0300

    feat: add arrow right icon to titles

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,6 +3,7 @@ import { Welcome } from "./components/welcome"
 import { BarbershopItem } from "./components/barbershop-item"
 import { Header } from "@/components/header"
 import { BookingItem } from "@/components/booking-item"
+import { Icons } from "@/components/icons"
 
 import { getAllBarbershops } from "@/actions/barbershop"
 
@@ -10,7 +11,7 @@ const Home = async () => {
   const barbershops = await getAllBarbershops()
 
   return (
-    <div className="max-w-6xl mx-auto container">
+    <div className="max-w-6xl mx-auto container mb-16">
       <Header />
 
       <div className="space-y-6 py-5 ">
@@ -24,7 +25,32 @@ const Home = async () => {
         </div>
 
         <div>
-          <p className="text-xs uppercase font-bold text-muted-foreground mb-3">Recomendados</p>
+          <div className="flex gap-2 items-center mb-3">
+            <p className="text-xs uppercase font-bold text-muted-foreground ">Recomendados</p>
+            <Icons.arrowRight className="w-4 h-4" />
+          </div>
+
+          <div className="flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+            {barbershops?.map((barbershop) => {
+              return (
+                <BarbershopItem
+                  key={barbershop.id}
+                  barbershop={{
+                      ...barbershop,
+                      imageUrl: barbershop.image_url
+                    }}
+                />
+              )
+            })}
+          </div>
+        </div>
+
+
+        <div>
+          <div className="flex gap-2 items-center mb-3">
+            <p className="text-xs uppercase font-bold text-muted-foreground ">Populares</p>
+            <Icons.arrowRight className="w-4 h-4" />
+          </div>
 
           <div className="flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
             {barbershops?.map((barbershop) => {
