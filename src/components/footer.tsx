f761d751682ba76fba547799422d18c42bd62096
João Manoel <joaomanoeldev@gmail.com>
2024-02-02 22:30:38 -0300

    feat: create footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,13 @@
+export const Footer = () => {
+  return (
+    <footer className="w-full flex items-center justify-center bg-muted py-6 px-5">
+      <a
+        href="https://github.com/JoaoManoelDev"
+        target="_blank"
+        className="text-muted-foreground opacity-75 underline"
+      >
+        Feito por João Manoel
+      </a>
+    </footer>
+  )
+}
