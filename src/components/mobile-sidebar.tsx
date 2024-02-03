9c57eb605051e48db9f64a3462db91f66c76079e
João Manoel <joaomanoeldev@gmail.com>
2024-02-03 19:17:58 -0300

    feat: create mobile sidebar componenet

---
+++ src/components/mobile-sidebar.tsx
@@ -0,0 +1,48 @@
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger
+} from "@/components/ui/sheet"
+import { Button } from "@/components/ui/button"
+import { Icons } from "@/components/icons"
+import { NavLink } from "@/components/nav-link"
+import { Separator } from "@/components/ui/separator"
+import { routes } from "@/constants/route"
+
+export const MobileSidebar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="ghost" size="icon" className="lg:hidden">
+          <Icons.menu />
+        </Button>
+      </SheetTrigger>
+
+      <SheetContent side="left" className="p-0">
+        <div className="h-full">
+          <SheetHeader className="text-left p-5">
+            <SheetTitle className="text-2xl">Menu</SheetTitle>
+          </SheetHeader>
+
+          <Separator />
+
+          <div className="space-y-2 leading-none mt-8 px-5">
+            {routes.map(route => (
+              <SheetClose key={route.href} asChild>
+                <NavLink href={route.href}>
+                  <div className="flex flex-1 items-center gap-2 py-3">
+                    <route.icon className="w-5 h-5" />
+                    <span className="text-xl">{route.label}</span>
+                  </div>
+                </NavLink>
+              </SheetClose>
+            ))}
+          </div>
+        </div>
+      </SheetContent>
+    </Sheet>
+  )
+}
