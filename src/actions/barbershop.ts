2bb6bdc3d2c18fd2968348a5f098c42ab66b04ce
João Manoel <joaomanoeldev@gmail.com>
2024-02-03 15:50:42 -0300

    feat: create get barbershop by id

--- src/actions/barbershop.ts
+++ src/actions/barbershop.ts
@@ -2,15 +2,46 @@
 
 import { db } from "@/lib/db"
 
+interface Services {
+  id: string
+  name: string
+  price: number
+  description: string
+  image_url: string
+  barbershop_id: string
+}
+
 interface Barbershop {
   id: string
   name: string
   address: string
   image_url: string
+  services: Services[]
 }
 
-export const getAllBarbershops = async () => {
-  const barbershops: Barbershop[] = await db.barbershop.findMany()
+export const getAllBarbershops = async (): Promise<Barbershop[]> => {
+  const barbershops = await db.barbershop.findMany()
 
   return barbershops
+}
+
+interface GetBarbershopByIdProps {
+  barbershopId: string,
+  includeServices?: boolean
+}
+
+export const getBarbershopById = async ({
+  barbershopId,
+  includeServices = false
+}: GetBarbershopByIdProps): Promise<Barbershop> => {
+  const barbershop = await db.barbershop.findUnique({
+    where: {
+      id: barbershopId
+    },
+    include: {
+      services: includeServices
+    }
+  })
+
+  return barbershop
 }
