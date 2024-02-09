d389051f814c36ca8dd442ef95db470af408800d
João Manoel <joaomanoeldev@gmail.com>
2024-02-09 16:37:34 -0300

    feat: create dateDayAndMonthFormatter and priceFormatter function

--- src/lib/formatters.ts
+++ src/lib/formatters.ts
@@ -2,3 +2,14 @@ import { format } from "date-fns"
 import { ptBR } from "date-fns/locale"
 
 export const dateFormatter = (date: Date) => format(date, "EEEE',' dd 'de' MMMM", { locale: ptBR })
+
+export const dateDayAndMonthFormatter = (date: Date) => format(date, "dd 'de' MMMM", { locale: ptBR })
+
+export const priceFormatter = (price: number) => {
+  const priceFormatted = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL"
+  }).format(price)
+
+  return priceFormatted
+}
