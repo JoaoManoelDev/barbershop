78a7031d80863ab142f0b3f8b52ed7e45f8bf96f
João Manoel <joaomanoeldev@gmail.com>
2024-02-09 01:00:28 -0300

    feat: add redirect to search params

--- src/components/auth/social.tsx
+++ src/components/auth/social.tsx
@@ -5,11 +5,15 @@ import { signIn } from "next-auth/react"
 import { Button } from "@/components/ui/button"
 import { Icons } from "@/components/icons"
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes"
+import { useSearchParams } from "next/navigation"
 
 export const Social = () => {
+  const searchParams = useSearchParams().get("callbackUrl")
+
   const onClick = (provider: "google") => {
     signIn(provider, {
-      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+      callbackUrl: searchParams || DEFAULT_LOGIN_REDIRECT,
+      redirect: true
     })
   }
 
