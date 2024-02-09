79f4bf0f6b2ca12b20d9ed157aa3e8991d54d018
João Manoel <joaomanoeldev@gmail.com>
2024-02-09 19:23:09 -0300

    feat: remove booking status

---
+++ prisma/migrations/20240209215812_remove_booking_status/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `status` on the `booking` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "booking" DROP COLUMN "status";
