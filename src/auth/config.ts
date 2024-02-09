e98fdf4d2bd63017097f5249056864fad62e2eb7
João Manoel <joaomanoeldev@gmail.com>
2024-02-09 01:05:11 -0300

    feat: active allowDangerousEmailAccountLinking

--- src/auth/config.ts
+++ src/auth/config.ts
@@ -6,7 +6,7 @@ export const authConfig = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      allowDangerousEmailAccountLinking: true
+      allowDangerousEmailAccountLinking: true,
     })
-  ]
+  ],
 } satisfies NextAuthConfig
