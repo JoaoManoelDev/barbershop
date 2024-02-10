cf753ddefdfdeffe58062a6935aeeba9a9856d9e
João Manoel <joaomanoeldev@gmail.com>
2024-02-10 13:02:06 -0300

    feat: add bookingItemProps

--- src/components/booking-item.tsx
+++ src/components/booking-item.tsx
@@ -6,21 +6,28 @@ import {
   AvatarImage
 } from "@/components/ui/avatar"
 
-export const BookingItem = () => {
+interface BookingItemProps {
+  booking: {
+    barbershopName: string
+    serviceName: string
+  }
+}
+
+export const BookingItem = ({ booking }: BookingItemProps) => {
   return (
     <Card>
       <CardContent className="p-5 grid grid-cols-3 py-0 pr-0">
         <div className="flex flex-col gap-2 py-5 col-span-2">
           <Badge className="hover:bg-primary w-fit">Confirmado</Badge>
 
-          <h2 className="font-bold">Corte de Cabelo</h2>
+          <h2 className="font-bold">{booking.serviceName}</h2>
 
           <div className="flex items-center gap-2">
             <Avatar className="w-6 h-6">
               <AvatarImage src="https://utfs.io/f/5832df58-cfd7-4b3f-b102-42b7e150ced2-16r.png" />
               <AvatarFallback>A</AvatarFallback>
             </Avatar>
-            <span className="text-small">Vintage barbar</span>
+            <span className="text-small">{booking.barbershopName}</span>
           </div>
         </div>
 
