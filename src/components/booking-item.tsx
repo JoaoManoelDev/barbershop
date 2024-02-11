190c338b47f8424bce9b64da770de259c5a5e7eb
João Manoel <joaomanoeldev@gmail.com>
2024-02-11 17:42:36 -0300

    feat: using formatters

--- src/components/booking-item.tsx
+++ src/components/booking-item.tsx
@@ -5,26 +5,41 @@ import {
   AvatarFallback,
   AvatarImage
 } from "@/components/ui/avatar"
+import {
+  dateDayFormatter,
+  dateHourAndMinutesFormatter,
+  dateIsFuture,
+  dateMonthFormatter
+} from "@/lib/formatters"
 
 interface BookingItemProps {
   booking: {
     barbershopName: string
+    barbershopImageUrl: string
     serviceName: string
+    bookingDate: Date
   }
 }
 
 export const BookingItem = ({ booking }: BookingItemProps) => {
+  const bookingConfirmed = dateIsFuture(booking.bookingDate)
+
   return (
     <Card>
       <CardContent className="p-5 grid grid-cols-3 py-0 pr-0">
         <div className="flex flex-col gap-2 py-5 col-span-2">
-          <Badge className="hover:bg-primary w-fit">Confirmado</Badge>
+          <Badge
+            className="hover:no w-fit"
+            variant={bookingConfirmed ? "default" : "secondary"}
+          >
+            {bookingConfirmed ? "Confirmado" : "Finalizado"}
+          </Badge>
 
           <h2 className="font-bold">{booking.serviceName}</h2>
 
           <div className="flex items-center gap-2">
             <Avatar className="w-6 h-6">
-              <AvatarImage src="https://utfs.io/f/5832df58-cfd7-4b3f-b102-42b7e150ced2-16r.png" />
+              <AvatarImage src={booking.barbershopImageUrl} className="object-cover" />
               <AvatarFallback>A</AvatarFallback>
             </Avatar>
             <span className="text-small">{booking.barbershopName}</span>
@@ -32,9 +47,9 @@ export const BookingItem = ({ booking }: BookingItemProps) => {
         </div>
 
         <div className="flex flex-col justify-center items-center border-l">
-          <p className="text-sm">Fevereiro</p>
-          <p className="text-2xl">06</p>
-          <p className="text-sm">17:45</p>
+          <p className="text-sm capitalize">{dateMonthFormatter(booking.bookingDate)}</p>
+          <p className="text-2xl">{dateDayFormatter(booking.bookingDate)}</p>
+          <p className="text-sm">{dateHourAndMinutesFormatter(booking.bookingDate)}</p>
         </div>
       </CardContent>
     </Card>
