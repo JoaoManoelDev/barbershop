e665618a83bf1db730b4c1594da3774b1c897572
João Manoel <joaomanoeldev@gmail.com>
2024-02-11 17:41:11 -0300

    feat: add dateMonthFormatter, dateDayFormatter, dateHourAndMinutesFormatter, dateIsFuture and dateIsPast formatter

--- src/lib/formatters.ts
+++ src/lib/formatters.ts
@@ -1,9 +1,19 @@
-import { format } from "date-fns"
+import { format, isFuture, isPast } from "date-fns"
 import { ptBR } from "date-fns/locale"
 
 export const dateFormatter = (date: Date) => format(date, "EEEE',' dd 'de' MMMM", { locale: ptBR })
 
 export const dateDayAndMonthFormatter = (date: Date) => format(date, "dd 'de' MMMM", { locale: ptBR })
+
+export const dateMonthFormatter = (date: Date) => format(date, "MMMM", { locale: ptBR })
+
+export const dateDayFormatter = (date: Date) => format(date, "dd")
+
+export const dateHourAndMinutesFormatter = (date: Date) => format(date, "hh:mm")
+
+export const dateIsFuture = (date: Date) => isFuture(date)
+
+export const dateIsPast = (date: Date) => isPast(date)
 
 export const priceFormatter = (price: number) => {
   const priceFormatted = new Intl.NumberFormat("pt-BR", {
