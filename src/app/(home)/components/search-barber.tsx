6a7d32d0aa7dafeddaf4c3958c4c12d88a112adb
João Manoel <joaomanoeldev@gmail.com>
2024-03-15 14:47:57 -0300

    feat: add logic to search barbershop

--- src/app/(home)/components/search-barber.tsx
+++ src/app/(home)/components/search-barber.tsx
@@ -1,14 +1,73 @@
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Icons } from "@/components/icons"
+"use client"
 
-export const SearchBarber = () => {
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { z } from "zod"
+
+import { Icons } from "@/components/icons"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+import { useRouter } from "next/navigation"
+
+const formSchema = z.object({
+  search: z
+    .string({
+      required_error: "Campo obrigatório.",
+    })
+    .trim()
+    .min(1, "Campo obrigatório."),
+})
+
+interface SearchProps {
+  defaultValues?: z.infer<typeof formSchema>
+}
+
+export const SearchBarber = ({ defaultValues }: SearchProps) => {
+  const router = useRouter()
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues,
+  })
+
+  const handleSubmit = (data: z.infer<typeof formSchema>) => {
+    router.push(`/barbershop?search=${data.search}`)
+  }
+
   return (
     <div className="flex items-center gap-2">
-      <Input placeholder="Busque por uma barbearia" />
-      <Button size="icon">
-        <Icons.search className="w-4 h-4" />
-      </Button>
+      <Form {...form}>
+        <form
+          className="flex w-full gap-4"
+          onSubmit={form.handleSubmit(handleSubmit)}
+        >
+          <FormField
+            control={form.control}
+            name="search"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input placeholder="Busque por uma barbearia..." {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit">
+            <Icons.search className="w-4 h-4" />
+          </Button>
+        </form>
+      </Form>
     </div>
   )
 }
